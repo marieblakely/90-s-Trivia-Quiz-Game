772134a2817f29feb682fd01986146f9da9fbcd6
Marie Blakely <marieblakely@myyahoo.com>
2024-04-01 16:26:51 -0400

    add question object to tvShowsQuestions array

--- data/questions.js
+++ data/questions.js
@@ -1,5 +1,5 @@
 const tvShowsQuestions = [
-  {}
+  {questionText: 'Which tv show featured six friends, one of which is a nerd, navigating the ups and downs of high school while frequently frustrating their principal?', correctAnswerText: 'Saved By The Bell', incorrectAnswer: ['Family Matters', 'Boy Meets World', 'All That']}
 ]
 
 const moviesQuestions = [
