fea9e0c03cefe43a0bf2a00a2e4ddc82943ee000
Marie Blakely <marieblakely@myyahoo.com>
2024-04-01 20:17:25 -0400

    complete tv shows questions objects

--- data/questions.js
+++ data/questions.js
@@ -1,5 +1,9 @@
 const tvShowsQuestions = [
-  {questionText: 'Which tv show featured six friends, one of which is a nerd, navigating the ups and downs of high school while frequently frustrating their principal?', correctAnswerText: 'Saved By The Bell', incorrectAnswerText: ['Family Matters', 'Boy Meets World', 'All That']}
+  {questionText: 'Which tv show featured six friends, one of which is a nerd, navigating the ups and downs of high school while frequently frustrating their principal?', correctAnswerText: 'Saved By The Bell', incorrectAnswerText: ['Family Matters', 'Boy Meets World', 'All That']},
+  {questionText: 'Which tv show featured a teenage male who got sent to live with Aunt and Uncle for getting in one little fight?', correctAnswerText: 'The Fresh Prince of Bel-air', incorrectAnswerText: ['Will Smith', 'Jazzy Fresh of Bel-air', 'Kenan & Kel']},
+  {questionText: 'In this tv show, twins separated at birth learn about each other as teenagers after running into each in the mall', correctAnswerText: 'Sister, Sister', incorrectAnswerText: ['Twins', 'The Suite Life of Zack & Cody', 'The Silent Twins']},
+  {questionText: 'What spin-off of The Cosby Show centered the lives of students at Hillman College?', correctAnswerText: 'A Different World', incorrectAnswerText: ['Boy Meets World', 'Felicity', 'Grown-ish']},
+  {questionText: 'What animated tv series focuses on a group of babies that find themselves in adventures both reqal and imaginary?', correctAnswerText: 'Rugrats', incorrectAnswerText: ['Little Bill', 'Sesame Street', 'Teletubbies']}
 ]
 
 const moviesQuestions = [
@@ -7,7 +11,7 @@ const moviesQuestions = [
 ]
 
 const toysQuestions = [
-  {questionText: 'What small, colorful handheld video game system was available in an erray of colors with a catalog of over 1,000 games?', correctAnswerText: 'Game Boy Color', incorrectAnswerText: ['Super Nintendo', 'Sega Genasis', 'Atari']},
+  {questionText: 'What small, colorful handheld video game system was available in an array of colors with a catalog of over 1,000 games?', correctAnswerText: 'Game Boy Color', incorrectAnswerText: ['Super Nintendo', 'Sega Genasis', 'Atari']},
   {questionText: 'Which of these handheld games calls out commands randomly that increases in speed as the game progresses?', correctAnswerText: 'Bop It', incorrectAnswerText: ['Simon Says', 'Wheel of Fortune', 'Game Gear']},
   {questionText: 'What keychain-sized virtual simulation game allows you to feed, and take care of your own pet?', correctAnswerText: 'Tamagotchi', incorrectAnswerText: ['Robot Dog', 'Hatchimal', 'Littlest Pet Shop']},
   {questionText: 'What toy gun uses requires manually pumping air to shoot water with power and range?', correctAnswerText: 'Super Soaker', incorrectAnswerText: ['Summer Soaker', 'Tyco Motorized Water Gun', 'Nerf Gun']},
