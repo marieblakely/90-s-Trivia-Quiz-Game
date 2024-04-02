cad0828ba9080a28caf360e0dce0ae4baf4e4413
Marie Blakely <marieblakely@myyahoo.com>
2024-04-02 16:27:40 -0400

    change color of headers

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ import { getRandomToyQuestion } from "../data/questions.js"
 import { getRandomMusicQuestion } from "../data/questions.js"
 
 
+
 /*-------------------------------- Variables --------------------------------*/
 
 
