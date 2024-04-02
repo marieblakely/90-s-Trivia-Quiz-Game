2d382e15d535741269ea22bac8500029fc0a0e9f
Marie Blakely <marieblakely@myyahoo.com>
2024-04-02 15:09:59 -0400

    fix grammatical errors in data file

--- data/questions.js
+++ data/questions.js
@@ -1,9 +1,9 @@
 const tvShowsQuestions = [
   {questionText: 'Which tv show featured six friends, one of which is a nerd, navigating the ups and downs of high school while frequently frustrating their principal?', correctAnswerText: 'Saved By The Bell', incorrectAnswerText: ['Family Matters', 'Boy Meets World', 'All That']},
-  {questionText: 'Which tv show featured a teenage male who got sent to live with Aunt and Uncle for getting in one little fight?', correctAnswerText: 'The Fresh Prince of Bel-air', incorrectAnswerText: ['Will Smith', 'Jazzy Fresh of Bel-air', 'Kenan & Kel']},
+  {questionText: 'Which tv show featured a teenage male who got sent to live with his Aunt and Uncle for getting in one little fight?', correctAnswerText: 'The Fresh Prince of Bel-air', incorrectAnswerText: ['Will Smith', 'Jazzy Fresh of Bel-air', 'Kenan & Kel']},
   {questionText: 'In this tv show, twins separated at birth learn about each other as teenagers after running into each in the mall', correctAnswerText: 'Sister, Sister', incorrectAnswerText: ['Twins', 'The Suite Life of Zack & Cody', 'The Silent Twins']},
   {questionText: 'What spin-off of The Cosby Show centered the lives of students at Hillman College?', correctAnswerText: 'A Different World', incorrectAnswerText: ['Boy Meets World', 'Felicity', 'Grown-ish']},
-  {questionText: 'What animated tv series focuses on a group of babies that find themselves in adventures both reqal and imaginary?', correctAnswerText: 'Rugrats', incorrectAnswerText: ['Little Bill', 'Sesame Street', 'Teletubbies']}
+  {questionText: 'What animated tv series focuses on a group of babies that find themselves in adventures both real and imaginary?', correctAnswerText: 'Rugrats', incorrectAnswerText: ['Little Bill', 'Sesame Street', 'Teletubbies']}
 ]
 
 const moviesQuestions = [
@@ -18,12 +18,12 @@ const toysQuestions = [
   {questionText: 'What small, colorful handheld video game system was available in an array of colors with a catalog of over 1,000 games?', correctAnswerText: 'Game Boy Color', incorrectAnswerText: ['Super Nintendo', 'Sega Genasis', 'Atari']},
   {questionText: 'Which of these handheld games calls out commands randomly that increases in speed as the game progresses?', correctAnswerText: 'Bop It', incorrectAnswerText: ['Simon Says', 'Wheel of Fortune', 'Game Gear']},
   {questionText: 'What keychain-sized virtual simulation game allows you to feed, and take care of your own pet?', correctAnswerText: 'Tamagotchi', incorrectAnswerText: ['Robot Dog', 'Hatchimal', 'Littlest Pet Shop']},
-  {questionText: 'What toy gun uses requires manually pumping air to shoot water with power and range?', correctAnswerText: 'Super Soaker', incorrectAnswerText: ['Summer Soaker', 'Tyco Motorized Water Gun', 'Nerf Gun']},
-  {questionText: 'Arguably the most popular game console of the nineties, this console had an incredible range of games such as Mario Bros. and Donkey Kong', correctAnswerText: 'Super Nintendo', incorrectAnswerText: ['Sega Genasis', 'Playstation', 'Game Cube']}
+  {questionText: 'What toy gun requires manually pumping air to shoot water with power and range?', correctAnswerText: 'Super Soaker', incorrectAnswerText: ['Summer Soaker', 'Tyco Motorized Water Gun', 'Nerf Gun']},
+  {questionText: 'Arguably the most popular game console of the nineties, this console had a large range of games that included Mario Bros. and Donkey Kong', correctAnswerText: 'Super Nintendo', incorrectAnswerText: ['Sega Genasis', 'Playstation', 'Game Cube']}
 ]
 
 const musicQuestions = [
-  {questionText: 'This crazy-sexy-cool trio sung about a guy not getting any love from from them on this hit', correctAnswerText: 'No Scrubs', incorrectAnswerText: ['TLC', 'Destinys Child', 'Bills, Bills, Bills']},
+  {questionText: 'A crazy-sexy-cool trio sung about a guy not getting any love from from them on this hit', correctAnswerText: 'No Scrubs', incorrectAnswerText: ['TLC', 'Destinys Child', 'Bills, Bills, Bills']},
   {questionText: 'The musical hit which featured the lyrics I want it that way was sung by what boy band?', correctAnswerText: 'Backstreet Boys', incorrectAnswerText: ['Blink 182', 'Nsync', 'LFO']},
   {questionText: 'This song by a young girl confessing her loneliness is killing her is named?', correctAnswerText: 'Baby One More Time', incorrectAnswerText: ['My Loneliness is Killing Me', 'Give Me A Sign', 'Brittney Spears']},
   {questionText: 'The California Love cali tempo rap song produced by Dr. Dre, performed by Tupac features which funk group?', correctAnswerText: 'Zapp' incorrectAnswerText: ['Earth Wind & Fire', 'The Parliament Funkadelic', 'The Gap Band']},
