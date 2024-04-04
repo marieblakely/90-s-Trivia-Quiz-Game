f0992632ad48904fe8bd4740c35b3e72efd07d8e
Marie Blakely <marieblakely@myyahoo.com>
2024-04-04 11:44:58 -0400

    category cards click

--- js/app.js
+++ js/app.js
@@ -15,14 +15,15 @@ const tvShowsQuestions = []
 /*------------------------ Cached Element References ------------------------*/
 
 const categoryCardContainer = document.querySelector('#category-card-container')
-const card = document.querySelector('.card')
-const card2 = document.querySelector('.card2')
-const card3 = document.querySelector('.card3')
-const card4 = document.querySelector('.card4')
+const card = document.querySelector('#card')
+const card2 = document.querySelector('#card2')
+const card3 = document.querySelector('#card3')
+const card4 = document.querySelector('#card4')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-card.addEventListener('click', tvQuestion)
+
+card.addEventListener('click', handleClick)
 card2.addEventListener('click', handleClick)
 card3.addEventListener('click', handleClick)
 card4.addEventListener('click', handleClick)
@@ -31,9 +32,15 @@ card4.addEventListener('click', handleClick)
 
 //when a category card is clicked, display a question from that category
 
-function tvQuestion() {
-  const newQuestion = getRandomTvShowQuestion()
-  tvShowsQuestions.push(newQuestion)
+function handleClick(evt) {
+  let categoryCard = evt.target
+  // make tv show card clickable
+  // if categoryCard.textContent === '90's movies' 
+  //call generateRand
+  //once tv show card is clicked get a random question from tvShowsQuestion array
+  // if card = getRandomTvShowQuestion(
+  // tvShowsQuestions.push(newQuestion)
+  console.log(categoryCard.textContent)
 }
 
 // function render(){
