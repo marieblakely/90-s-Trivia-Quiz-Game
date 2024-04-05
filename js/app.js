7235881b2db1d3e907737ccb6aa7b650b1b37d0e
Marie Blakely <marieblakely@myyahoo.com>
2024-04-05 12:11:55 -0400

    define state variables

--- js/app.js
+++ js/app.js
@@ -8,39 +8,47 @@ import { getRandomMusicQuestion } from "../data/questions.js"
 
 /*-------------------------------- Variables (state)-----------------------------*/
 
-
-
+const tvShowsQuestion = []
+const moviesQuestions = []
+const toysQuestions = []
+const musicQuestions = []
+let questionIndex = 0
 
 /*------------------------ Cached Element References ---------------------------*/
 
 const categoryCardContainer = document.querySelector('#category-card-container')
-const card = document.querySelector('#card')
-const card2 = document.querySelector('#card2')
-const card3 = document.querySelector('#card3')
-const card4 = document.querySelector('#card4')
+const cardEls = document.querySelector('#card')
+const cardEl2= document.querySelector('#card2')
+const cardEl3 = document.querySelector('#card3')
+const cardEl4 = document.querySelector('#card4')
 
 /*----------------------------- Event Listeners --------------------------------*/
 
 
-card.addEventListener('click', tvShows)
-card2.addEventListener('click', handleClick)
-card3.addEventListener('click', handleClick)
-card4.addEventListener('click', handleClick)
+cardEls.addEventListener('click', tvShows)
+// card2.addEventListener('click', handleClick)
+// card3.addEventListener('click', handleClick)
+// card4.addEventListener('click', handleClick)
 
 /*-------------------------------- Functions --------------------------------*/
 
 
-//when a category card is clicked, display a question from that category
+//when a category card is clicked
 function tvShows(evt) {
   let card = evt.target;
   // let card = 'tvShowsQuestions'
   // let card2 = 'moviesQuestions'
   // make tv show card clickable
-  if ((categoryCard.textContent) === ('tvShowsQuestions')){
+  if ((card.textContent) === ('tvShows')){
+    // append ('questionText')
+    // getRandomTvShowQuestion()
   }
     //once tv show card is clicked get a random question from tvShowsQuestion array
   
-  console.log(categoryCard.textContent)
+  console.log(card.textContent)
 }
 
+// create new question function
+
+//append function
 
