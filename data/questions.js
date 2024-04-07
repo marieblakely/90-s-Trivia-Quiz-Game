5cbe077e9c9d56799be0700ce608ce4f6feb679d
Marie Blakely <marieblakely@myyahoo.com>
2024-04-07 17:37:52 -0400

    update correct answer and answers for each array of objects

--- data/questions.js
+++ data/questions.js
@@ -1,33 +1,33 @@
 const tvShowsQuestions = [
-  {questionText: 'Which tv show featured six friends, one of which is a nerd, navigating the ups and downs of high school while frequently frustrating their principal?', correctAnswerText: 'Saved By The Bell', incorrectAnswerText1: 'Family Matters', incorrectAnswerText2:'Boy Meets World', incorrectAnswerText3:'All That'},
-  {questionText2: 'Which tv show featured a teenage male who got sent to live with his Aunt and Uncle for getting in one little fight?', correctAnswerText: 'The Fresh Prince of Bel-air', incorrectAnswerText: ['Will Smith', 'Jazzy Fresh of Bel-air', 'Kenan & Kel']},
-  {questionText: 'In this tv show, twins separated at birth learn about each other as teenagers after running into each in the mall', correctAnswerText: 'Sister, Sister', incorrectAnswerText: ['Twins', 'The Suite Life of Zack & Cody', 'The Silent Twins']},
-  {questionText: 'What spin-off of The Cosby Show centered the lives of students at Hillman College?', correctAnswerText: 'A Different World', incorrectAnswerText: ['Boy Meets World', 'Felicity', 'Grown-ish']},
-  {questionText: 'What animated tv series focuses on a group of babies that find themselves in adventures both real and imaginary?', correctAnswerText: 'Rugrats', incorrectAnswerText: ['Little Bill', 'Sesame Street', 'Teletubbies']}
+  {questionText: 'Which tv show featured six friends, one of which is a nerd, navigating the ups and downs of high school while frequently frustrating their principal?', correctAnswer: 'Saved By The Bell', answers:['Family Matters', 'All That', 'Saved By The Bell', 'Boy Meets World']},
+  {questionText: 'Which tv show featured a teenage male who got sent to live with his Aunt and Uncle for getting in one little fight?', correctAnswer: 'The Fresh Prince of Bel-air', answers: ['Will Smith', 'Jazzy Fresh of Bel-air', 'Kenan & Kel', 'The Fresh Prince of Bel-air']},
+  {questionText: 'In this tv show, twins separated at birth learn about each other as teenagers after running into each in the mall', correctAnswer: 'Sister, Sister', answers: ['Twins','Sister, Sister', 'The Suite Life of Zack & Cody', 'The Silent Twins']},
+  {questionText: 'What spin-off of The Cosby Show centered the lives of students at Hillman College?', correctAnswer: 'A Different World', answers: ['Boy Meets World', 'Felicity', 'Grown-ish', 'A Different World']},
+  {questionText: 'What animated tv series focuses on a group of babies that find themselves in adventures both real and imaginary?', correctAnswer: 'Rugrats', answers: ['Little Bill', 'Sesame Street', 'Teletubbies', 'Rugrats']}
 ]
 
 const moviesQuestions = [
-  {questionText: 'A businessman and an escort fall hard for one another in what movie?', correctAnswerText: 'Pretty Woman', incorrectAnswerText: ['Great Expectations', 'Cruel Intentions', 'Indescent Exposure']},
-  {questionText: 'A beautiful, popular, and rich high school student befriends a new student and gives her a makeover in this movie', correctAnswerText: 'Clueless', incorrectAnswerText: ['She All That', 'Grease', 'Princess Diaries']},
-  {questionText: 'In what movie does a young boy have to fend for himself when his parents mistakenly leave him behind on a famiy trip?', correctAnswerText: 'Home Alone', incorrectAnswerText: ['Dennis the Menace', 'Problem Child', 'Jumanji']},
-  {questionText: 'A teenage girl is terrorized by a masked killer with a knife who targets her and her friends by using scary movies in which movie?', correctAnswerText: 'Scary Movie', incorrectAnswerText: ['I Know What You Did Last Summer', 'Urban Legend', 'Texas Chainsaw Massacre']},
-  {questionText: 'In this movie an extraterrestrial attack worldwide results in a group of people launching a counterattack on July 4', correctAnswerText: 'Independence Day', incorrectAnswerText: ['The Fifth Element', 'Men In Black', 'Predator II']}
+  {questionText: 'A businessman and an escort fall hard for one another in what movie?', correctAnswer: 'Pretty Woman', answers: ['Great Expectations', 'Cruel Intentions', 'Indescent Exposure']},
+  {questionText: 'A beautiful, popular, and rich high school student befriends a new student and gives her a makeover in this movie', correctAnswer: 'Clueless', answers: ['She All That', 'Grease', 'Princess Diaries']},
+  {questionText: 'In what movie does a young boy have to fend for himself when his parents mistakenly leave him behind on a famiy trip?', correctAnswer: 'Home Alone', answers: ['Dennis the Menace', 'Problem Child', 'Jumanji']},
+  {questionText: 'A teenage girl is terrorized by a masked killer with a knife who targets her and her friends by using scary movies in which movie?', correctAnswer: 'Scary Movie', answers: ['I Know What You Did Last Summer', 'Urban Legend', 'Texas Chainsaw Massacre']},
+  {questionText: 'In this movie an extraterrestrial attack worldwide results in a group of people launching a counterattack on July 4', correctAnswer: 'Independence Day', answers: ['The Fifth Element', 'Men In Black', 'Predator II']}
 ]
 
 const toysQuestions = [
-  {questionText: 'What small, colorful handheld video game system was available in an array of colors with a catalog of over 1,000 games?', correctAnswerText: 'Game Boy Color', incorrectAnswerText: ['Super Nintendo', 'Sega Genasis', 'Atari']},
-  {questionText: 'Which of these handheld games calls out commands randomly that increases in speed as the game progresses?', correctAnswerText: 'Bop It', incorrectAnswerText: ['Simon Says', 'Wheel of Fortune', 'Game Gear']},
-  {questionText: 'What keychain-sized virtual simulation game allows you to feed, and take care of your own pet?', correctAnswerText: 'Tamagotchi', incorrectAnswerText: ['Robot Dog', 'Hatchimal', 'Littlest Pet Shop']},
-  {questionText: 'What toy gun requires manually pumping air to shoot water with power and range?', correctAnswerText: 'Super Soaker', incorrectAnswerText: ['Summer Soaker', 'Tyco Motorized Water Gun', 'Nerf Gun']},
-  {questionText: 'Arguably the most popular game console of the nineties, this console had a large range of games that included Mario Bros. and Donkey Kong', correctAnswerText: 'Super Nintendo', incorrectAnswerText: ['Sega Genasis', 'Playstation', 'Game Cube']}
+  {questionText: 'What small, colorful handheld video game system was available in an array of colors with a catalog of over 1,000 games?', correctAnswer: 'Game Boy Color', answers: ['Super Nintendo', 'Sega Genasis', 'Atari']},
+  {questionText: 'Which of these handheld games calls out commands randomly that increases in speed as the game progresses?', correctAnswer: 'Bop It', answers: ['Simon Says', 'Wheel of Fortune', 'Game Gear']},
+  {questionText: 'What keychain-sized virtual simulation game allows you to feed, and take care of your own pet?', correctAnswer: 'Tamagotchi', answers: ['Robot Dog', 'Hatchimal', 'Littlest Pet Shop']},
+  {questionText: 'What toy gun requires manually pumping air to shoot water with power and range?', correctAnswer: 'Super Soaker', answers: ['Summer Soaker', 'Tyco Motorized Water Gun', 'Nerf Gun']},
+  {questionText: 'Arguably the most popular game console of the nineties, this console had a large range of games that included Mario Bros. and Donkey Kong', correctAnswer: 'Super Nintendo', answers: ['Sega Genasis', 'Playstation', 'Game Cube']}
 ]
 
 const musicQuestions = [
-  {questionText: 'A crazy-sexy-cool trio sung about a guy not getting any love from from them on this hit', correctAnswerText: 'No Scrubs', incorrectAnswerText: ['TLC', 'Destinys Child', 'Bills, Bills, Bills']},
-  {questionText: 'The musical hit which featured the lyrics I want it that way was sung by what boy band?', correctAnswerText: 'Backstreet Boys', incorrectAnswerText: ['Blink 182', 'Nsync', 'LFO']},
-  {questionText: 'This song by a young girl confessing her loneliness is killing her is named?', correctAnswerText: 'Baby One More Time', incorrectAnswerText: ['My Loneliness is Killing Me', 'Give Me A Sign', 'Brittney Spears']},
-  {questionText: 'The California Love cali tempo rap song produced by Dr. Dre, performed by Tupac features which funk group?', correctAnswerText: 'Zapp', incorrectAnswerText: ['Earth Wind & Fire', 'The Parliament Funkadelic', 'The Gap Band']},
-  {questionText: 'This song performed by a Canadian superstar was featured on the movie The Titanic', correctAnswerText: 'My Heart Will Go On', incorrectAnswerText: ['I Will Always Love You', 'I Have Had The Time Of My Life', 'Do Not Let Go']}
+  {questionText: 'A crazy-sexy-cool trio sung about a guy not getting any love from from them on this hit', correctAnswer: 'No Scrubs', answers: ['TLC', 'Destinys Child', 'Bills, Bills, Bills']},
+  {questionText: 'The musical hit which featured the lyrics I want it that way was sung by what boy band?', correctAnswer: 'Backstreet Boys', answers: ['Blink 182', 'Nsync', 'LFO']},
+  {questionText: 'This song by a young girl confessing her loneliness is killing her is named?', correctAnswer: 'Baby One More Time', answers: ['My Loneliness is Killing Me', 'Give Me A Sign', 'Brittney Spears']},
+  {questionText: 'The California Love cali tempo rap song produced by Dr. Dre, performed by Tupac features which funk group?', correctAnswer: 'Zapp', answers: ['Earth Wind & Fire', 'The Parliament Funkadelic', 'The Gap Band']},
+  {questionText: 'This song performed by a Canadian superstar was featured on the movie The Titanic', correctAnswer: 'My Heart Will Go On', answers: ['I Will Always Love You', 'I Have Had The Time Of My Life', 'Do Not Let Go']}
 ]
 
 function getRandomTvShowQuestion (){
@@ -50,5 +50,9 @@ export {
   getRandomTvShowQuestion,
   getRandomMovieQuestion,
   getRandomToyQuestion,
-  getRandomMusicQuestion
+  getRandomMusicQuestion,
+  tvShowsQuestions,
+  moviesQuestions,
+  toysQuestions,
+  musicQuestions
 }
--- js/app.js
+++ js/app.js
@@ -3,26 +3,29 @@ import { getRandomTvShowQuestion } from "../data/questions.js"
 import { getRandomMovieQuestion } from "../data/questions.js"
 import { getRandomToyQuestion } from "../data/questions.js"
 import { getRandomMusicQuestion } from "../data/questions.js"
-
+import { moviesQuestions, toysQuestions, tvShowsQuestions, musicQuestions } from "../data/questions.js"
 
 
 /*-------------------------------- Variables (state)-----------------------------*/
 
-const tvShowsQuestions = []
-const moviesQuestions = []
-const toyQuestions = []
-const musicQuestions = []
+// const tvShowsQuestions = []
+// const moviesQuestions = []
+// const toyQuestions = []
+// const musicQuestions = []
+let currentQuestionList = []
 let currentQuestionTextIndex = 0
 let gameIsInPlay
+let score
 
 /*------------------------ Cached Element References ---------------------------*/
 
 const categoryCardContainerEl = document.querySelector('#category-card-container')
+const questionCardContainerEl = document.querySelector('#question-card-container')
 const cardEl = document.querySelector('#card')
 const cardEl2= document.querySelector('#card2')
 const cardEl3 = document.querySelector('#card3')
 const cardEl4 = document.querySelector('#card4')
-const resetButtonContainer = document.querySelector('.reset-button')
+const resetButtonContainer = document.querySelector('.reset-button-container')
 const resetBtn = document.getElementById('reset')
 const nextQuestionBtn = document.getElementById('next-question')
 
@@ -34,80 +37,121 @@ cardEl2.addEventListener('click', createMovieQuestion)
 cardEl3.addEventListener('click', createToyQuestion)
 cardEl4.addEventListener('click', createMusicQuestion)
 resetBtn.addEventListener('click', handleReset)
-nextQuestionBtn.addEventListener('click', nextQuestion)
+// nextQuestionBtn.addEventListener('click', nextQuestion)
   
 
 /*-------------------------------- Functions --------------------------------*/
 
-// init()
+init()
 
 
 function createTvQuestion(){
-  const newTvQuestion = getRandomTvShowQuestion()
-  tvShowsQuestions.push(newTvQuestion)
+  // const newTvQuestion = getRandomTvShowQuestion()
+  // tvShowsQuestions.push(newTvQuestion)
+  currentQuestionList = [...tvShowsQuestions]
+  gameIsInPlay = true
   render()
 }
 
 function createMovieQuestion(){
-  const newMovieQuestion = getRandomMovieQuestion()
-  moviesQuestions.push(newMovieQuestion)
+  // const newMovieQuestion = getRandomMovieQuestion()
+  // moviesQuestions.push(newMovieQuestion)
+  currentQuestionList = [...moviesQuestions]
+  gameIsInPlay = true
   render()
 }
 
 function createToyQuestion(){
-  const newToyQuestion = getRandomToyQuestion()
-  toyQuestions.push(newToyQuestion)
+  // const newToyQuestion = getRandomToyQuestion()
+  // toyQuestions.push(newToyQuestion)
+  currentQuestionList = [...toysQuestions]
+  gameIsInPlay = true
   render()
 }
 
 function createMusicQuestion(){
-  const newMusicQuestion = getRandomMusicQuestion()
-  musicQuestions.push(newMusicQuestion)
+  // const newMusicQuestion = getRandomMusicQuestion()
+  // musicQuestions.push(newMusicQuestion)
+  currentQuestionList = [...musicQuestions]
+  gameIsInPlay = true
   render()
 }
 
-function nextQuestion (){
-  // for (let i =0; i < tvShowsQuestions.length; i++) {
+function selectAnswer (evt){
+  console.log(evt.target.textContent)
+  if (evt.target.textContent === currentQuestionList[currentQuestionTextIndex].correctAnswer) {
+    console.log('correct answer')
+    score += 1
+    // increase score by 1
+
+  } else {
+    console.log('incorrect answer')
+  }
   currentQuestionTextIndex += 1
-  if (currentQuestionTextIndex === tvShowsQuestions.length) {
-    currentQuestionTextIndex += 0
-  } 
-  appendQuestion(currentQuestionTextIndex)
+  if (currentQuestionTextIndex < currentQuestionList.length) {
+
+    render()
+  } else {
+    console.log(`You got ${score} out of ${currentQuestionList.length} correct!`)
+
+  }
+  // for (let i =0; i < tvShowsQuestions.length; i++) {
+  // currentQuestionTextIndex += 1
+  // if (currentQuestionTextIndex === tvShowsQuestions.length) {
+  //   currentQuestionTextIndex += 0
+  // } 
+  // appendQuestion(currentQuestionTextIndex)
+
 }
 
 function render() {
+  console.log(currentQuestionList)
   if (gameIsInPlay) {
+    resetButtonContainer.style.display = ''
+    categoryCardContainerEl.style.display = 'none'
+    questionCardContainerEl.style.display = ''
+    appendQuestion()
+  } else {
     resetButtonContainer.style.display = 'none'
+    questionCardContainerEl.style.display = 'none'
     categoryCardContainerEl.style.display = ''
-  }  
-  categoryCardContainerEl.innerHTML = ''
-  tvShowsQuestions.forEach((question, idx) => {
-    appendQuestion(question, idx)
-    })
-  moviesQuestions.forEach((question, idx) => {
-    appendQuestion(question, idx)
-    }) 
-  toyQuestions.forEach((question, idx) => {
-    appendQuestion(question, idx)
-    }) 
-  musicQuestions.forEach((question, idx) => {
-    appendQuestion(question, idx)
-    })        
+
+  }
+  // categoryCardContainerEl.innerHTML = ''
+  // tvShowsQuestions.forEach((question, idx) => {
+  //   appendQuestion(question, idx)
+  //   })
+  // moviesQuestions.forEach((question, idx) => {
+  //   appendQuestion(question, idx)
+  //   }) 
+  // toysQuestions.forEach((question, idx) => {
+  //   appendQuestion(question, idx)
+  //   }) 
+  // musicQuestions.forEach((question, idx) => {
+  //   appendQuestion(question, idx)
+  //   })        
   }
 
 
-function appendQuestion(tvShowsQuestions, idx){
+function appendQuestion(){
+  questionCardContainerEl.innerHTML = ''
   let questionCard = document.createElement('div')
   // let answerCard = document.createElement('div')
-  questionCard.className = `card ${tvShowsQuestions.questionText}`
+  questionCard.className = `card ${currentQuestionList[currentQuestionTextIndex].questionText}`
   // answerCard.className = `card ${answer.correctAnswerText}`
   questionCard.innerHTML = 
     `<div>
-      <p>${tvShowsQuestions.questionText}</p>
+      <p>${currentQuestionList[currentQuestionTextIndex].questionText}</p>
     </div>
     `
     
-  categoryCardContainerEl.appendChild(questionCard)
+  questionCardContainerEl.appendChild(questionCard)
+  currentQuestionList[currentQuestionTextIndex].answers.forEach(answer => {
+    let answerBtn = document.createElement('button')
+    answerBtn.textContent = answer
+    answerBtn.addEventListener('click', selectAnswer)
+    questionCard.appendChild(answerBtn)
+  })
   // answerCard.innerHTML = 
   //   `<div>
   //   <p>${answer.correctAnswerText1}</p>
@@ -118,6 +162,7 @@ function appendQuestion(tvShowsQuestions, idx){
 
 function init(){
   gameIsInPlay = false
+  score = 0
   render()
 }
 
@@ -125,5 +170,4 @@ function handleReset (){
   gameIsInPlay = false
   render()
 }
-
 
